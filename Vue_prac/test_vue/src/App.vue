69119e2ab1d984dfb567561a628684158ab306a6
dongchol <49258960+dongchol@users.noreply.github.com>
2023-08-30 21:10:13 +0900

    Add files via upload

---
+++ Vue_prac/test_vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="jdc_prac" class="demo">
+    <p> 값 선택</p>
+    <select v-model="select_box">
+      <option
+        v-for="(item, index) in selectList"
+        :key="index"
+        :value="item.value"
+        >{{ item.name }}</option
+      >
+    </select>
+    <br>
+    <br>
+    <span> 출력 값: {{ select_box }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      select_box: "",
+      selectList: [
+        { name: "선택 창", value: "" },
+        { name: "1", value: "첫번째 값" },
+        { name: "2", value: "두번째 값" },
+        { name: "3", value: "세번째 값" },
+        { name: "4", value: "네번째 값" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue_prac/test_vue/src/components/comp_test02.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h4> comp_test02_export </h4>
+        <ul>
+            <h4>{{ msg }}</h4>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TEST_script2',
+  props: {
+    msg: String
+  }
+}
+</script>
