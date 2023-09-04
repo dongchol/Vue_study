8aceb2735203fac76a0463eddf150292a7fe5c9b
charle_JO <dongchol801@naver.com>
2023-09-04 21:36:17 +0900

    bootstrap_menu_todo_merge

---
+++ vue_bstp/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "vue/multi-word-component-names": ["error", {
+            "ignores": ["default", "Login", "Home", "About"]
+          }],
+        'vue/no-deprecated-slot-attribute': 'off'
+    },
+}
--- vue_bstp/src/App.vue
+++ vue_bstp/src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-  </div> 
+  <nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 shadow-lg sticky-top">
+      <div class="container"> 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<!--id값을 햄버거 코드에 data-bs-target과 같게 해야 이 div안에 있는 메뉴를 인식하여 햄버거에 넣는다-->
+
+<!--네비게이션 바 안에 들어가는 메뉴를 꾸며주는 코드-->
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link">
+                        HOME
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/about" class="nav-link">
+                        About
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <a href="#services" class="nav-link">
+                        services
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#gallary" class="nav-link">
+                        gallaery
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#cta" class="nav-link">
+                        connact
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#find-us" class="nav-link">
+                        find us
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </nav>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -31,6 +53,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
